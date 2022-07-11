69ea1b82986778b04950433956a8f6a3a10b3ee3
flexsidee <flexsidee@gmail.com>
2022-07-11 04:08:52 +0100

    started working on google auth

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Chat from "./Components/Chat/Chat";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import Login from "./Components/Login/Login";
 
 function App() {
+	const [user, setUser] = useState(null);
+
 	return (
 		<div className="app">
-			<div className="app__body">
-				<Router>
-					<Sidebar />
-					<Routes>
-						<Route path="/" element={<Chat />} />
-						<Route path="/rooms/:roomId" element={<Chat />} />
-					</Routes>
-				</Router>
-			</div>
+			{!user ? (
+				<Login />
+			) : (
+				<div className="app__body">
+					<Router>
+						<Sidebar />
+						<Routes>
+							<Route path="/" element={<Chat />} />
+							<Route path="/rooms/:roomId" element={<Chat />} />
+						</Routes>
+					</Router>
+				</div>
+			)}
 		</div>
 	);
 }
